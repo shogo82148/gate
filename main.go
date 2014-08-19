d112292458c8cf40722d75883bef1d1048fdb259
Ichinose Shogo <shogo82148@gmail.com>
2014-08-19 15:50:17 +0900

    show version name

--- main.go
+++ main.go
@@ -2,17 +2,28 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 )
 
-var (
-	confFile = flag.String("conf", "config.yml", "config file path")
+const (
+	Version = "0.2"
 )
 
 func main() {
+	var confFile string
+	var versionFlag bool
+	flag.StringVar(&confFile, "conf", "config.yml", "config file path")
+	flag.BoolVar(&versionFlag, "version", false, "show version")
+	flag.BoolVar(&versionFlag, "v", false, "show version")
 	flag.Parse()
 
-	conf, err := ParseConf(*confFile)
+	if versionFlag {
+		fmt.Println(Version)
+		return
+	}
+
+	conf, err := ParseConf(confFile)
 	if err != nil {
 		panic(err)
 	}
